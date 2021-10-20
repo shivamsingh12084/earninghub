77b180c282d6aa310807508a212e596eac61b4a5
shivamsingh12084 <shivamsingh12084@gmail.com>
2021-10-20 11:09:52 +0530

    Main Page Creation

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Local
-    'accounts'
+    'accounts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
